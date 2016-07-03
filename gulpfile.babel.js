82bd1cfad717a402264c171bb774752b6cd93549
Xiyang Chen <settinghead@gmail.com>
2016-07-02 20:03:59 -0400

    fix

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -131,7 +131,7 @@ gulp.task('scripts', () =>
       .pipe($.sourcemaps.write())
       .pipe(gulp.dest('.tmp/scripts'))
       .pipe($.concat('main.min.js'))
-      .pipe($.uglify({preserveComments: 'some'}))
+      // .pipe($.uglify({preserveComments: 'some'}))
       // Output files
       .pipe($.size({title: 'scripts'}))
       .pipe($.sourcemaps.write('.'))
