5256bd0467f0bb4e9d2b547484b5a5a1ded3a89d
Xiyang Chen <settinghead@gmail.com>
2016-07-03 18:31:18 -0400

    partial fix frosty

--- app/scripts/effects.js
+++ app/scripts/effects.js
@@ -9,72 +9,6 @@
 /* globals TimelineMax:false */
 /* eslint max-len: 0 */
 
-$(function() {
-  var flightpath = {
-    entry: {
-      curviness: 1.25,
-      autoRotate: true,
-      values: [
-          {x: 100, y: -20},
-          {x: 300, y: 10}
-      ]
-    },
-    looping: {
-      curviness: 1.25,
-      autoRotate: true,
-      values: [
-          {x: 510, y: 60},
-          {x: 620, y: -60},
-          {x: 500, y: -100},
-          {x: 380, y: 20},
-          {x: 500, y: 60},
-          {x: 580, y: 20},
-          {x: 620, y: 15}
-      ]
-    },
-    leave: {
-      curviness: 1.25,
-      autoRotate: true,
-      values: [
-          {x: 660, y: 20},
-          {x: 800, y: 130},
-          {x: $(window).width() + 300, y: -100}
-      ]
-    }
-  };
-  // init controller
-  var controller = new ScrollMagic.Controller();
-
-  // create tween
-  var tween = new TimelineMax()
-    .add(TweenMax.to($('#petal1'), 1.2, {css: {bezier: flightpath.entry}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal1'), 2, {css: {bezier: flightpath.looping}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal1'), 1, {css: {bezier: flightpath.leave}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal2'), 1.2, {css: {bezier: flightpath.entry}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal2'), 2, {css: {bezier: flightpath.looping}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal2'), 1, {css: {bezier: flightpath.leave}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal3'), 1.2, {css: {bezier: flightpath.entry}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal3'), 2, {css: {bezier: flightpath.looping}, ease: Power1.easeInOut}))
-    .add(TweenMax.to($('#petal3'), 1, {css: {bezier: flightpath.leave}, ease: Power1.easeInOut}));
-
-  // build scene
-  new ScrollMagic.Scene({triggerElement: '#trigger', duration: 1000, offset: 100})
-          .setPin('#target1')
-          .setTween(tween)
-          // .addIndicators() // add indicators (requires plugin)
-          .addTo(controller);
-  new ScrollMagic.Scene({triggerElement: '#trigger', duration: 1000, offset: 0})
-          .setPin('#target2')
-          .setTween(tween)
-          // .addIndicators() // add indicators (requires plugin)
-          .addTo(controller);
-  new ScrollMagic.Scene({triggerElement: '#trigger', duration: 1000, offset: -100})
-          .setPin('#target3')
-          .setTween(tween)
-          // .addIndicators() // add indicators (requires plugin)
-          .addTo(controller);
-});
-
 // for smooth scrolling
 
 $(function() {
@@ -108,7 +42,10 @@ function recheckHeader() {
 
 // frosty glass
 
+var topOffset = 0;
+
 $(function() {
+  topOffset = $(window).scrollTop();
   html2canvas($('main'), {
     onrendered: function(canvas) {
       $('.blurheader').append(canvas);
@@ -119,7 +56,7 @@ $(function() {
             0,
             $('canvas').width(),
             $('canvas').height(),
-            20);
+            15);
     }
   });
   var vv = setTimeout(function() {
@@ -128,24 +65,18 @@ $(function() {
   }, 200);
 });
 
-$(window).scroll(function() {
-  $('canvas').css(
-        '-webkit-transform',
-        'translatey(-' + $(window).scrollTop() + 'px)');
-});
+$(window).scroll(adjustPane);
 
 window.onresize = function() {
   $('canvas').width($(window).width());
 };
 
-$(document).bind('touchmove', function() {
-  $('canvas').css(
-        '-webkit-transform',
-        'translatey(-' + $(window).scrollTop() + 'px)');
-});
+$(document).bind('touchmove', adjustPane);
+$(document).bind('touchend', adjustPane);
 
-$(document).bind('touchend', function() {
+function adjustPane() {
+  console.log($(window).scrollTop() - topOffset);
   $('canvas').css(
         '-webkit-transform',
-        'translatey(-' + $(window).scrollTop() + 'px)');
-});
+        'translatey(-' + ($(window).scrollTop() - topOffset) + 'px)');
+}
